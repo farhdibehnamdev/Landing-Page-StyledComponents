82db6a38e26e6d1d62e13f36bc8fbb36b5dbfef4
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-23 19:05:40 +0430

    🎉Initial commit.

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { StyledHeader } from "./styles/Header.Styled";
+
+const Header = function () {
+  return (
+    <StyledHeader bg="red">
+      <h1>Huddle</h1>
+    </StyledHeader>
+  );
+};
+
+export default Header;
