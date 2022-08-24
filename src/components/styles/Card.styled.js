de8e14eedd676d0daf279775ecd65b8bfe483c97
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-24 15:33:10 +0430

    💄 Added Card section.

---
+++ src/components/styles/Card.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledCard = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 60px;
+  margin: 40px 0;
+  border-radius: 15px;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+`;
